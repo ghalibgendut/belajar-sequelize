5c4705ac9f94fa6dff2ebeabb90858e904ae03dc
ghalibgendut <ghalibsasmito@gmail.com>
2022-03-16 14:30:39 +0700

    add where search condition

--- src/book/bookController.js
+++ src/book/bookController.js
@@ -1,3 +1,5 @@
+const { Op } = require("sequelize");
+const Sequelize = require('sequelize')
 const book = require('./bookModel')
 
 class BookController {
@@ -32,6 +34,37 @@ class BookController {
             res.status(500).json({message: 'Error!', err})
         }
     } 
+
+    static searchBook = async (req, res) => {
+        try {
+            let isbn = req.query.isbn
+            let name = req.query.name
+            let bookData
+            
+            if (!isbn && !name) {
+                bookData = await book.findAll()
+
+                res.status(200).json({message: 'Data Retrived', bookData})
+            }
+            else{
+                bookData = await book.findAll({
+                    where: {
+                        [Op.or]: [{isbn: isbn}, {name: {[Op.like]: `%${name}%`}}]
+                    }
+                })
+    
+                if (!bookData) {
+                    res.status(404).json({message:`Data not found!`})
+                }
+                else{
+                    res.status(200).json({bookData})
+                }
+            }
+            
+        } catch (err) {
+            res.status(500).json({message: 'Error!', err})
+        }
+    }
 }
 
 module.exports = BookController
