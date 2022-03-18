69853d3ef3a443ae1357553c02a98140183a1ec0
ghalibgendut <ghalibsasmito@gmail.com>
2022-03-18 14:58:18 +0700

    add update function book

--- src/book/bookController.js
+++ src/book/bookController.js
@@ -1,19 +1,7 @@
-const { Op } = require("sequelize");
-const Sequelize = require('sequelize')
+const { Op, where } = require("sequelize");
 const book = require('./bookModel')
 
 class BookController {
-
-    static getAllData = async (req, res) => {
-        try {
-            const bookData = await book.findAll()
-
-            res.status(200).json({message: 'Data Retrived', bookData})
-            
-        } catch (err) {
-            res.status(500).json({message: 'Error!', err})
-        }
-    }
 
     static addBook = async (req, res) => {
         try {
@@ -61,6 +49,49 @@ class BookController {
                 }
             }
             
+        } catch (err) {
+            res.status(500).json({message: 'Error!', err})
+        }
+    }
+
+    static updateBookData = async (req, res) => {
+        try {
+            const id = req.params.id ? req.params.id : res.status(400).json({message: `ID cannot be null!`})
+            let data = {
+                isbn: req.body.isbn ? req.body.isbn : "",
+                name: req.body.name ? req.body.name : "",
+                year: req.body.year ? req.body.year : "",
+                author: req.body.author ? req.body.author : "",
+                description: req.body.description ? req.body.description : ""
+            }
+
+            // check if data not null
+            if (data.isbn == '' && data.name == '' && data.year == '' && data.author == '' && data.description == '') {
+                res.status(400).json({message: `Data cannot be null!`})
+            }
+            else{
+                // call the data from db
+                const bookDb = await book.findAll({where: {id}})
+                if (!bookDb) {
+                    res.status(404).json({message: `No data found!`})
+                }
+                else {
+                    let bookObj = bookDb[0].dataValues
+
+                    const updateData = await book.update({
+                        isbn: data.isbn ? data.isbn : bookObj.isbn,
+                        name: data.name ? data.name : bookObj.name,
+                        year: data.year ? data.year : bookObj.year,
+                        author: data.author ? data.author : bookObj.author,
+                        description: data.description ? data.description : bookObj.description,
+                        updatedAt: Date.now()
+                    },{where: {id}})
+    
+                    await updateData.save()
+
+                    res.status(200).json({message:`Update data success!`})
+                }
+            }
         } catch (err) {
             res.status(500).json({message: 'Error!', err})
         }
--- src/book/bookRoutes.js
+++ src/book/bookRoutes.js
@@ -2,9 +2,9 @@ const express = require('express')
 const app = new express.Router()
 const bookController = require('./bookController')
 
-app.get('/all-book-data', bookController.getAllData)
 app.get('/search-book',bookController.searchBook)
 app.post('/add-book', bookController.addBook)
+app.patch('/book-update/:id', bookController.updateBookData)
 
 
 
